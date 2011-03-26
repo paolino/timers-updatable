6d3e476a30d7416b424cc930092e5989a1b3d058
Paolo Veronelli <paolo.veronelli@gmail.com>
2011-03-26 19:58:32 +0100

    added a replacer timer

--- System/Timer/Updatable.hs
+++ System/Timer/Updatable.hs
@@ -4,7 +4,7 @@
 --
 -- Parallel and serial update politics are implemented.
 --
--- In the example we start a timer with a time to wait of 10 seconds, hang 2 threads which will wait for it to finish and update 
+-- In the example we start a timer with a time to wait of 10 seconds, hang 2 threads which will wait for it to finish, and update 
 -- it after 5 seconds to wait for other 6 seconds. 
 -- It will complete and run its action and the hanged threads after 11 seconds because of its parallel nature. 
 -- The serial timer would have ringed after 16 seconds.
@@ -35,18 +35,23 @@ module System.Timer.Updatable
   -- * Builders
   , parallel
   , serial
+  , replacer
+  -- * Utility
+  , longThreadDelay
   ) where
 
+import Data.List (unfoldr)
 import Data.Maybe
+import Data.Int (Int64)
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Monad (when, forever)
 import Control.Concurrent.STM
 import Control.Concurrent.Killable
 
 -- | A delay in microseconds
-type Delay = Int
+type Delay = Int64
 
--- | Abstract timers that can be updated. Hanging via wait function can be done by any number of threads, which is sinchronization.
+-- | Abstract timers that can be updated. Hanging via wait function can be done by any number of threads, which is synchronization.
 data Updatable a = Updatable {
   wait :: STM (Maybe a),     -- ^ wait until the timer rings, or signal Nothing if timer is destroyed
   renew :: Delay -> STM (),  -- ^ update the delay in the timer
@@ -77,7 +82,8 @@ engine k t w d0 = do
     (writeTChan deltas) 
     (k >> kill [p,z] >> atomically (putTMVar x Nothing))
 
--- | Create and start a parallel updatable timer. This timer renew actions will start parallel timers. Last timer that is over will compute the given action.
+-- | Create and start a parallel updatable timer. The "renew" action for this timer will start parallel timers. The last timer
+-- that is over will compute the given action.
 parallel  :: IO a             -- ^ the action to run when timer rings 
           -> Delay            -- ^ time to wait
           -> IO (Updatable a) -- ^ the updatable parallel timer
@@ -86,7 +92,7 @@ parallel a d0 = do
   tp <- newTVarIO []
   let 
     t k = do 
-      p <- forkIO $ atomically (readTVar tz >>= writeTVar tz . (+1)) >> threadDelay k >> atomically (readTVar tz >>= writeTVar tz . (subtract 1))
+      p <- forkIO $ atomically (readTVar tz >>= writeTVar tz . (+1)) >> longThreadDelay k >> atomically (readTVar tz >>= writeTVar tz . (subtract 1))
       atomically $ readTVar tp >>= writeTVar tp . (p :)
     w = do 
       atomically $ do
@@ -96,7 +102,8 @@ parallel a d0 = do
     k = atomically (readTVar tp) >>= kill 
   engine k t w d0
          
--- | Create and start a serial updatable timer. This timer renew action will schedule new timer after the running one. The timer will run the given action after the sum of all scheduled times is over.
+-- | Create and start a serial updatable timer. The "renew" action for this timer will schedule new timer after the running one.
+-- The timer will run the given action after the sum of all scheduled times is over.
 serial  :: IO a             -- ^ the action to run when timer rings 
           -> Delay            -- ^ time to wait
           -> IO (Updatable a) -- ^ the updatable parallel timer
@@ -108,8 +115,43 @@ serial a d0 = do
       l <- atomically $ (Just `fmap` readTChan tz) `orElse` return Nothing
       case l of
         Nothing -> a
-        Just l -> threadDelay l >> w
+        Just l -> longThreadDelay l >> w
   engine (return ()) t w d0
+
+-- | Create  and start a replacer updatable timer.  The "renew" action for this timer will insert a new timer replacing the running one.
+-- The timer will run the given action after this time
+
+replacer  :: IO a             -- ^ the action to run when timer rings 
+          -> Delay            -- ^ time to wait
+          -> IO (Updatable a) -- ^ the updatable parallel timer
+replacer a d0 = do
+  tz <- newTVarIO []
+  z <- newEmptyTMVarIO 
+  let 
+    t k = do
+      ps <- atomically $ readTVar tz
+      mapM_ kill ps
+      p <- forkIO $ longThreadDelay k >> atomically (putTMVar z ())
+      atomically $ readTVar tz >>= writeTVar tz . (p:)
+    w = atomically (takeTMVar z) >> a
+  engine (return ()) t w d0
+
+
+-- | Pause the thread for the given number of microseconds.  There is no guarantee that the thread will be restarted promptly
+-- after the delay, but it will not be started before then.
+-- 
+-- Similar to "threadDelay", but takes a 64-bit argument.  The Haskell 2010 specification says that (maxBound :: Int) is at least 
+-- 2^29-1.  However 2^29 microseconds is only about 538 seconds.  GHC on a 32-bit machine has a 32 bit Int, but that is still less
+-- than 36 minutes.  64-bit signed integers give a maximum delay of over 292 million years, which should be sufficient.
+longThreadDelay :: Delay -> IO ()
+longThreadDelay d = mapM_ (threadDelay . fromIntegral) $ unfoldr f d
+   where
+      f d1 | d1 <= 0     = Nothing
+           | d1 < maxInt = Just (d1, 0)
+           | otherwise   = Just (maxInt, d1-maxInt)
+      maxInt = fromIntegral (maxBound :: Int)  -- Platform-dependent
+
+
 
 main = do
  t <- parallel (return 5) $ 10^7
